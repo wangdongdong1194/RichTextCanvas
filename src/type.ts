4749ac352f1c342932ed54a1eb6354c10a0b6280
王志东 <32211411+wangdongdong1194@users.noreply.github.com>
2026-05-19 18:39:45 +0800

    Create type.ts

---
+++ src/type.ts
@@ -0,0 +1,50 @@
+export interface TextStyle {
+  fontSize: number;
+  fontFamily: string;
+  color: string;
+}
+
+export interface StyledToken {
+  value: string;
+  style: TextStyle;
+}
+
+export interface CaretPosition {
+  x: number;
+  y: number;
+  lineIndex: number;
+  lineHeight: number;
+}
+
+export interface LayoutChar {
+  index: number;
+  x: number;
+  y: number;
+  width: number;
+  lineIndex: number;
+}
+
+export interface LayoutLine {
+  index: number;
+  y: number;
+  height: number;
+  start: number;
+  end: number;
+  chars: LayoutChar[];
+}
+
+export type VerticalAlign = "top" | "center" | "bottom";
+
+export interface EditorOptions {
+  width: number;
+  height: number;
+  maxCanvasWidth: number;
+  maxCanvasHeight: number;
+  padding: number;
+  lineGap: number;
+  background: string;
+  selectionColor: string;
+  caretColor: string;
+  defaultStyle: TextStyle;
+  verticalAlign: VerticalAlign;
+}
